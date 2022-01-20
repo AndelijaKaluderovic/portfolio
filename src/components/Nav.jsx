f11d210bb89074a6d5a805503fb53b31e7a906f3
AndelijaKaluderovic <andeka011@gmail.com>
2022-01-20 15:30:14 +0100

    projects responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
                 </div>
                 <div className="right">
                     <nav className="navigation">
-                        <ul>
+                        <ul className="navigation-list">
                             <li><a href="#home">Home</a></li>
                             <li><a href="#about">About</a></li>
                             <li><a href="#work">Work</a></li>
